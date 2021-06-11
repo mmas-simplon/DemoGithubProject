403280f6b410b149ab52e52849f0e6a99cec487b
Adrien <adrnsmn@gmail.com>
2021-06-11 15:16:45 +0200

    Ajout d'une LoginViewModelServices

---
+++ DemoGithubProject/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  DemoGithubProject
+//
+//  Created by Admin on 11/06/2021.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Ma vue de login")
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
