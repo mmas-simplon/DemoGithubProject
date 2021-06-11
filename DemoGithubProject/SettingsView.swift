91baba771d4b77ad4487160c7439d3a7af504f09
Adrien <adrnsmn@gmail.com>
2021-06-11 15:21:31 +0200

    J'ai fini la vue de settings.

---
+++ DemoGithubProject/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  DemoGithubProject
+//
+//  Created by Admin on 11/06/2021.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Ceci est une vue de settings.")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
