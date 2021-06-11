3aa21d72738373ea97972aee5bc0f6d729073ba3
Mickaël MAS <appiwedia@gmail.com>
2021-06-11 14:14:54 +0200

    Initial Commit

---
+++ DemoGithubProject/DemoGithubProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoGithubProjectApp.swift
+//  DemoGithubProject
+//
+//  Created by Mickael Mas on 11/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct DemoGithubProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
