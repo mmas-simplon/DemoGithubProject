d6b58cf1c9ef630ecdaece4f8ecc1481b39d4a41
Mickaël MAS <appiwedia@gmail.com>
2021-06-11 14:37:35 +0200

    Creation Model User

---
+++ DemoGithubProject/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  DemoGithubProject
+//
+//  Created by Mickael Mas on 11/06/2021.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let email: String
+    let isBG: Bool = true
+}
